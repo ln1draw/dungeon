8ce7b58399f806eecce0e346482d32d32d61f549
shanghaiellen@gmail.com <shanghaiellen@gmail.com>
2013-10-31 12:19:29 -0700

    added a few methods and the player class, plus some dungeons

--- dungeon.rb
+++ dungeon.rb
@@ -6,23 +6,6 @@ class Dungeon
     @rooms = []
   end
 
-  class Room
-    attr_accessor :reference, :name, :description, :connections
-
-    def intialize(reference, name, description, connections)
-      @reference = reference
-      @name = name
-      @description = description
-      @connections = connections
-    end
-
-    def full_description
-      @name + "\n\nYou are in " + @description
-    end
-  end
-
-  Player = Struct.new(:name, :location)
-
   def add_room(reference, name, description, connections)
     @rooms << Room.new(reference, name, description, connections)
   end
@@ -40,6 +23,43 @@ class Dungeon
     @rooms.detect{|room| room.reference==reference}    
   end
 
+  def find_room_in_direction(direction)
+    find_room_in_dungeon(@player.location).connections[direction]
+  end
 
+  def go(direction)
+    puts "You go " + direction.to_s
+    @player.location = find_room_in_direction(direction)
+    show_current_description
+  end
+
+  class Player
+    attr_accessor :name,   :location
+
+    def initialize(name)
+      @name=name
+    end
+  end
+
+
+  class Room
+    attr_accessor :reference, :name, :description, :connections
+
+    def intialize(reference, name, description, connections)
+      @reference = reference
+      @name = name
+      @description = description
+      @connections = connections
+    end
+
+    def full_description
+      @name + "\n\nYou are in " + @description
+    end
+  end
 end
-#this is a comment 
+
+my_dungeon = Dungeon.new("Ellen McAwesomesauce")
+my_dungeon.add_room(:largecave, "Large Cave", "EPICALLY large cave", {:west => :smallcave})
+my_dungeon.add_room(:smallcave, "Small Cave", "This cave is teeny-tiny", {:east => :largecave})
+
+my_dungeon.start(:largecave)
